cd3423b7e64df38809040c5b838b7aebcce71f6d
MAK TRNKA <maktrnka@comcast.net>
2024-03-11 23:14:23 -0700

    Changes

--- src/BestBooks.jsx
+++ src/BestBooks.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './BestBooks.css'; // Import the CSS file
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -8,21 +10,52 @@ class BestBooks extends React.Component {
     }
   }
 
-  /* TODO: Make a GET request to your API to fetch all the books from the database  */
+  componentDidMount() {
+    // Make a GET request to your server's /books route
+    fetch('/books')
+      .then(response => response.json())
+      .then(data => {
+        // Store the book data returned from the server in the application state
+        this.setState({ books: data });
+      })
+      .catch(error => console.error('Error fetching books:', error));
+  }
 
   render() {
-
-    /* TODO: render all the books in a Carousel */
-
     return (
       <>
-        <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
+        <h2>My Essential Lifelong Learning & Formation Shelf</h2>
 
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
+        {this.state.books.length > 0 ? (
+          // Render books using Bootstrap carousel
+          <div id="bookCarousel" className="carousel slide" data-bs-ride="carousel">
+            <div className="carousel-inner">
+              {this.state.books.map((book, index) => (
+                <div key={index} className={`carousel-item ${index === 0 ? 'active' : ''}`}>
+                  <img src={book.imageUrl} className="d-block w-100" alt={book.title} />
+                  <div className="carousel-caption d-none d-md-block">
+                    <h5>{book.title}</h5>
+                    <p>{book.author}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#bookCarousel" data-bs-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#bookCarousel" data-bs-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Next</span>
+            </button>
+          </div>
         ) : (
-          <h3>No Books Found :(</h3>
+          // Render a message if no books are found
+          <h3>No Books Found</h3>
         )}
+
+        {/* Link to the About page */}
+        <Link to="./About" className="link-about">About</Link>
       </>
     )
   }
